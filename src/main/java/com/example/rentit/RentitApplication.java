14fa9cee50a3cc8863ad96595c4f42b3228b00d6
Ismael Martinez <ismaelmartinezc>
2019-05-14 23:28:11 -0400

    Initial commit

---
+++ src/main/java/com/example/rentit/RentitApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentitApplication.class, args);
+	}
+
+}
